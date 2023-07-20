07a4679d6cac4de3ffd356af4f28cdfbe44095a6
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-07-20 15:44:28 +0530

    index page updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import Footer from './Footer'
 import Banner from './Banner'
 import Feature from './Feature'
 import News from './News'
+import Media from './Media'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Feature />
       <hr className='sm:hidden block border-t-1 border-gray-300 mt-4 w-10/12 ml-8' />
       <News />
+      <Media/>
       <Footer />
     </div>
   )
--- src/components/Home/Media.jsx
+++ src/components/Home/Media.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
-import news from '../../assets/news.jpg'
 import { motion } from 'framer-motion'
 
-const News = () => {
+const Media = () => {
     return (
         <>
-            <div className="max-w-screen-xl mx-auto sm:mt-10 sm:mb-8">
+            <div className="max-w-screen-xl mx-auto sm:mt-16 sm:mb-16">
                 <div className="container m-auto px-6">
 
                     <div className="md:flex justify-between items-center">
@@ -14,28 +13,24 @@ const News = () => {
                             <motion.div initial={{ y: "-10vw", opacity: 0 }}
                                 whileInView={{ y: 0, opacity: 1, transition: { type: "spring", stiffness: 50, delay: 0.4 } }}
                                 viewport={{ once: true, amount: 0.8 }} className="sm:pt-6 pb-4 space-y-2">
-                                <h1 className="text-5xl font-bold">News</h1>
-                                <p className="text-base text-[#545454] ">See, what’s new from Firstlook. <br />
+                                <h1 className="text-4xl sm:text-5xl font-bold text-[#3A3737] font-oxygen">Media</h1>
+                                <h1 className="text-4xl sm:text-5xl font-bold text-[#3A3737] font-oxygen">Say’s about us !</h1>
+                                <p className="text-base text-[#545454] pt-6">See, what’s new from Firstlook. <br />
                                     News and updates from us.
                                 </p>
                             </motion.div>
                             <motion.div initial={{ y: "-10vw", opacity: 0 }}
                                 whileInView={{ y: 0, opacity: 1, transition: { type: "spring", stiffness: 50, delay: 0.8 } }}
                                 viewport={{ once: true, amount: 0.8 }}
-                                className="md:w-6/12 order-2 xl:ml-20 sm:hidden block">
-                                <img src={news} alt="" />
+                                className="md:w-6/12 order-2 xl:ml-20 sm:hidden block sm:pt-0 pt-8">
+                                <img src='https://imagetolink.com/ib/lfhleKwS9Z.png' alt="" />
                             </motion.div>
-                            <motion.hr initial={{ y: "-10vw", opacity: 0 }}
-                                whileInView={{ y: 0, opacity: 1, transition: { type: "spring", stiffness: 50, delay: 0.5 } }}
-                                viewport={{ once: true, amount: 0.8 }} className='border-t-1 border-gray-300 sm:mt-1.5 mt-6 sm:w-[435px]' />
+
                             <motion.div initial={{ y: "-10vw", opacity: 0 }}
                                 whileInView={{ y: 0, opacity: 1, transition: { type: "spring", stiffness: 50, delay: 0.6 } }}
-                                viewport={{ once: true, amount: 0.8 }} className="pt-5 sm:pt-5 space-y-2">
-                                <span className="text-[12px]">13 JULY 2023</span>
-                                <h1 className="text-[21px] font-bold sm:w-[337px] pt-3 sm:pt-0">‘Firstlook’ introduced the web version in India</h1>
-                                <p className='pt-3 sm:w-[435px] text-justify pb-6 sm:pb-4'>Launched ‘Firstlook’ a social netwrok for singles to find life partner. The web version is now introduced in India in the domain called www.firstlok.pro</p>
-                                <Link className="inline-flex items-center py-2 space-x-2 bg-[#FC3657] text-[12px] px-5 rounded-md text-white">
-                                    MORE NEWS
+                                viewport={{ once: true, amount: 0.8 }} className="pt-10 sm:pt-20 space-y-2">
+                                <Link className="inline-flex items-center py-4 space-x-2 border border-[#DDDCDC] text-[16px] px-14 rounded-lg">
+                                    Get Media Kit
                                 </Link>
                             </motion.div>
                         </div>
@@ -43,7 +38,7 @@ const News = () => {
                             whileInView={{ y: 0, opacity: 1, transition: { type: "spring", stiffness: 50, delay: 0.8 } }}
                             viewport={{ once: true, amount: 0.8 }}
                             className="md:w-6/12 order-2 xl:ml-20 sm:block hidden">
-                            <img src={news} className='border-2' alt="" />
+                            <img src='https://imagetolink.com/ib/lfhleKwS9Z.png' alt="" />
                         </motion.div>
                     </div>
                 </div>
@@ -52,4 +47,4 @@ const News = () => {
     )
 }
 
-export default News
+export default Media
