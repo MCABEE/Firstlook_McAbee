470e24b8e891c82eed2ec9a5f0d939749e87cb21
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-07-26 18:26:01 +0530

    pricing page updated

--- src/components/Pricing/PricingPage.jsx
+++ src/components/Pricing/PricingPage.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react"
 import Footer from "../Home/Footer"
 import Navbar from "../Home/Navbar"
 import BoostProfile from "./BoostProfile"
 import Pricing from "./Pricing"
 
 const PricingPage = () => {
+    useEffect(() => {
+        window.scrollTo(0,0)
+      }, [])
     return (
         <>
             <Navbar />
