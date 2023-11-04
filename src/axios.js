1c1d41789f47bfedbb92dc73c4b6a9271ef5f7f7
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-11-04 22:39:35 +0530

    base url updated

--- src/axios.js
+++ src/axios.js
@@ -4,9 +4,9 @@ const instance = axios.create({
 
     // baseURL: "http://localhost:3500/"
 
-    // baseURL: "https://api.firstlook.pro/"
+    baseURL: "https://api.firstlook.pro/"
 
-    baseURL: "https://testapi.firstlook.pro"
+    // baseURL: "https://testapi.firstlook.pro"
 })
 
 export default instance
